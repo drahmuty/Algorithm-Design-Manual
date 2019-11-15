714c431407ad75fa022cbcc6c56e0377cd4b423a
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-11-14 22:03:19 -0600

    Update 01-02 "The Trip".py

--- 01-02 "The Trip".py
+++ 01-02 "The Trip".py
@@ -1,1 +1,10 @@
+# n = number of students (we can eliminate this variable and just use the length of the list)
+# exp = list of expenses for each student
 
+def trip(exp):
+    y = 0
+    avg = sum(exp) / len(exp)
+    for e in exp:
+        if e < avg:
+            y += avg - e
+    return y
