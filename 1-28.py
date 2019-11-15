48d20418301ca96b42e29306a42c7b1362cb45e6
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-11-14 21:36:59 -0600

    Update 1-28.py

--- 1-28.py
+++ 1-28.py
@@ -1,1 +1,16 @@
 # Write a function to perform integer division without using either / or * operators. Find a fast way to do it.
+
+def div(n, d):
+    ans = 0
+    if d == 0:
+        return ans
+    if (n < 0 or d < 0):
+        sign = -1
+    else:
+        sign = 1
+    n = abs(n)
+    d = abs(d)
+    while(n >= d):
+        n -= d
+        ans += 1
+    return sign * ans
