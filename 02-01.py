830ce790aa3062d4be406ea37f60c1acac156cc3
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-11-19 21:16:25 -0600

    Update 02-01.py

--- 02-01.py
+++ 02-01.py
@@ -1,7 +1,7 @@
 def mystery(n):
     r = 0
-    for i in range(1, n-1):
-        for j in range(i+1, n):
-            for k in range(1, j):
+    for i in range(1, n-1+1):
+        for j in range(i+1, n+1):
+            for k in range(1, j+1):
                 r += 1
     return r
