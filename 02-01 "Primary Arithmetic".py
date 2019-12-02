7500cb0cac31977118a6ea6687a73447e52008c8
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-12-01 18:15:55 -0600

    Update 02-01 "Primary Arithmetic".py

--- 02-01 "Primary Arithmetic".py
+++ 02-01 "Primary Arithmetic".py
@@ -1,1 +1,14 @@
-
+def carry(x, y):
+    count = 0
+    c = 0
+    while (x > 0) or (y > 0):
+        a = x % 10
+        b = y % 10
+        if (a + b + c) > 9:
+            c = 1
+            count += 1
+        else:
+            c = 0
+        x //= 10
+        y //= 10
+    return count
