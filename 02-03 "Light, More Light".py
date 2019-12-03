abfcdadef07b0906fd9f1eb24dec0077fdc61b46
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-12-02 22:21:28 -0600

    Update 02-03 "Light, More Light".py

--- 02-03 "Light, More Light".py
+++ 02-03 "Light, More Light".py
@@ -1,1 +1,18 @@
+# O(n) algorithm
+def light(n):
+    x = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            x += 1
+    if x % 2 == 0:
+        return 'off'
+    else:
+        return 'on'
 
+# O(1) algorithm - I looked up this solution and translated it from C to Python
+def light(n):
+    s = round(sqrt(n))
+    if s * s == n:
+        return 'on'
+    else:
+        return 'off'
