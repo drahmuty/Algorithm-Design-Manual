6e8fea18f28b81db0ac887c2c2180ee45316a3c7
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-12-02 20:54:12 -0600

    Update 02-02 "A Multiplication Game".py

--- 02-02 "A Multiplication Game".py
+++ 02-02 "A Multiplication Game".py
@@ -1,59 +1,21 @@
 def game(n):
     p = 1
     winner = 'stan'
-    nums = [2,3,4,5,6,7,8,9]
-    numslen = len(nums)
-    count = 0
-    while p < n:
-        p *= nums[count % numslen]
+    while True:
+        p *= strategy(p, n)
+        if p >= n:
+            break
         if winner == 'stan':
             winner = 'ollie'
         else:
-            winner == 'ollie':
-                winner = 'stan'
+            winner = 'stan'
     return winner
-
+            
 def strategy(p, n):
-    # return the most strategic multiplier
-    nums = [2,3,4,5,6,7,8,9]
-    for num in nums:
-        if (num * p) >= n:
-            return num
-    for num in nums:
-        q = num * p
-        q = q * strategy(q, n)
-        if (num * q) >= n:
-            return num
-        
-#     for a in nums:
-#         for b in nums:
-#             if (a * b * strategy((p * a * b), n)) >= n:
-#                 return a
-
-from math import ceil, log
-
-def st(p, n):
-    for x in range(9,1,-1):
-        y = ceil(log(n,x))
-        if (y % 2) != 0:
+    for x in range(2, 10):
+        if (x * p) >= n:
             return x
-        
-        
-        
-def st(p, n):
-    print('p='+str(p)+', n='+str(n))
-    nums = [2,3,4,5,6,7,8,9]
-    for num in nums:
-        if (num * p) >= n:
-            print('found: '+str(num))
-            return num
-    for num in nums:
-        print('try: '+str(num))
-        q = num * p
-        q = q * st(q, n)
-        print('q='+str(q))
-        if num * q >= n:
-            return num
-        elif (9 * q) < n:
-            return num
-        
+    for y in range(9, 1, -1):
+        if (y * p * 9) < n:
+            return y
+    return 2
