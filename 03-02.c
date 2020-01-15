d1a29ccf9dd2b9679e50887a6b4204e5eed410c9
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-01-14 21:08:10 -0600

    Update 03-02.c

--- 03-02.c
+++ 03-02.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
 
+struct list {
+    item_type item;
+    struct list *next;
+} list;
+
+int main(void)
+{
+    return 0;
+}
+
+void reverse(*list) {
+    *p;
+    last = NULL;
+    
+    if (list->next == NULL) {
+        # set item as the root
+        return;
+    }
+    else {
+        last = list;
+        
+    }
+}
+
+/*
+Writing psuedo code instead.
+
+Given a linked list...
+*p;
+last = NULL;
+while (l->next != NULL) {
+    p = l;
+    l->next = last;
+    }
+    
+
+
+
+*/
