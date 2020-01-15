d78d3165baa665d6e2754b437a8f0cb32be6dd15
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-01-14 20:44:42 -0600

    Update 03-01.py

--- 03-01.py
+++ 03-01.py
@@ -1,1 +1,19 @@
+# Program to check for proper parantheses pairing and nesting
 
+def foo(x):
+    # make sure there are the same total number of ( and )
+    # make sure ( comes before )
+    string = str(x)
+    c = l = r = 0
+    for s in string:
+        c += 1
+        if s == '(':
+            l += 1
+        elif s == ')':
+            r += 1
+        if r > l:
+            return 'error at ' + str(c)
+    if l == r:
+        return 'valid'
+    else:
+        return 'error: missing one or more )'
