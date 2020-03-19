cd62a1f955069f9ec48bdf73bb1092e71eaa2535
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-03-19 16:54:38 -0500

    Update 03-10.py

--- 03-10.py
+++ 03-10.py
@@ -1,1 +1,35 @@
+"""
+For best-fit scenario:
+- Each bucket is a node on the tree. 
+- For each new item, find the maximum node that can hold the new item. This takes O(n log n) time worst case.
+- Add the new item to that node and update the total value of the node.
+- Rebalance the tree.
+- Return the total number of nodes as the final step. This takes O(n) time.
 
+For worst-fit scenario:
+- For each new item, find the minimum node that can hold the new item. This takes O(n log n) time worst case.
+- Add the new item to that node and update the total value of the node.
+- Rebalance the tree.
+- Return the total number of nodes as the final step. This takes O(n) time.
+"""
+
+def main(items, tree, total):
+    for item in items:
+        bestfit(item, tree, total)
+    countnodes(tree)
+
+def bestfit(item, tree, total):
+    if item + tree.value == total:
+        tree.value = item + tree.value
+    elif item + tree.value > total:
+        if item > tree.value:
+            if tree.right is None:
+                tree.right = Tree(item)
+            else:
+                bestfit(item, tree.right, total)
+        if tree.left is None:
+            tree.left = Tree(item)
+        else:
+            bestfit(item, tree.left, total)
+    else:
+        if 
