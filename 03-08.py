e5883e40b142dccf0999b01dff219a6b6e1330ce
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-03-18 20:37:57 -0500

    Update 03-08.py

--- 03-08.py
+++ 03-08.py
@@ -1,1 +1,8 @@
+''' We want to use a priority queue implemented as a binary tree. 
 
+Insert is just inserting into the tree.
+
+Member is just searching for an item in the tree.
+
+Deleting the kth smallest element is finding the minimum, then finding k-1 successors.
+'''
