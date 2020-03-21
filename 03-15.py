62aab59de9a65ed9ddd010da4ad56f2f14c171e9
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-03-21 16:17:24 -0500

    Update 03-15.py

--- 03-15.py
+++ 03-15.py
@@ -18,3 +18,29 @@ def delete(x, y):
         y[x] = None
     else:
         return 'not found'
+
+    
+    
+# Looked up correct solution and modified it for Python
+
+A = [None] * 100
+B = [None] * 100   
+k = 0
+
+def insert(x):
+    global k
+    A[x] = k
+    B[k] = x
+    k = k + 1
+    return
+
+def search(x):
+    global k
+    return A[x] < k and B[A[x]] == x
+
+def delete(x):
+    global k
+    k -= 1
+    A[B[k]] = A[x]
+    B[A[x]] = B[k]
+    return
