22e78bdf104dbe8d039b719032970399d205f10e
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-03-21 16:00:25 -0500

    Update 03-15.py

--- 03-15.py
+++ 03-15.py
@@ -1,1 +1,20 @@
+A = []
+B = []
 
+for r in range(0, 100):
+    A[r] = 0
+
+def insert(x, y):
+    y[x] = x
+
+def search(x, y):
+    if y[x] == x:
+        return x
+    else:
+        return None
+
+def delete(x, y):
+    if search(x, y) is not None:
+        y[x] = None
+    else:
+        return 'not found'
