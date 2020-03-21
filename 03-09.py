b7ae740f7c4ae1adf1f63331729c3c323d43934b
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-03-21 15:33:42 -0500

    Update 03-09.py

--- 03-09.py
+++ 03-09.py
@@ -1,1 +1,32 @@
+class Tree():
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    def show(self):
+        out = 'val = ' + str(self.value) + ', '
+        out += 'left = ' + str(self.left) + ', '
+        out += 'right = ' + str(self.right)
+        print(out)
 
+def insert(x, tree):
+    if tree is None:
+        tree = x
+    elif x.value < tree.value:
+        if tree.left == None:
+            tree.left = x
+        else:
+            insert(x, tree.left)
+    else:
+        if tree.right == None:
+            tree.right = x
+        else:
+            insert(x, tree.right)
+
+def traverse(tree):
+    if tree == None:
+        return
+    traverse(tree.left)
+    print(tree.value)
+    traverse(tree.right)
