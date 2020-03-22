0a3733defb3c44340669a3ba6ee9565c2162cceb
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-03-21 21:39:35 -0500

    Update 03-16.py

--- 03-16.py
+++ 03-16.py
@@ -1,110 +1,104 @@
 # Characters to skip
 skip = [' ', '\n', '\t', '.', ',', ':', ';', '?', '!']
 
-# Main function
-def words(file):
-    with open(file) as file_object:
-        contents = file_object.read().strip()
+# Main function (second version, using object oriented programming)
+def main():
+    doc = Doc('file.txt')
+    words = LinkedList()
+    w = doc.next_word()
+    while w:
+        if not words.search(w):
+            words.insert(w)
+        w = doc.next_word()
+    words.array()
+    return
+
+# Document class
+class Doc():
+    def __init__(self, filename):
+        with open(filename) as file_obj:
+            self.contents = file_obj.read().strip()
+        self.index = 0
+        self.length = len(self.contents)
+
+    def next_word(self):
         word = ''
-        for c in contents:
+        stop = False
+        while self.index < self.length:
+            c = self.contents[self.index]
             if c in skip:
-                print(word)
-                # insert(word)
-                word = ''
+                if stop:
+                    return word
+                self.index += 1
                 continue
-            else:
-                word += c
-
-# words('file.txt')
-
-
-# Linked list
-class linked_list():
+            stop = True
+            word += c
+            self.index += 1
     
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-    
-    def insert(self, value):
-        if self.next == None:
-            self.next = linked_list(value)
-        else:
-            self.next.insert(value)
-
-    def search(self, value):
-        if self.value == value:
-            return True
-        elif self.next == None:
-            return False
-        else:
-            return self.next.search(value)
-
-    def show(self):
-        if self.next == None:
-            print(self.value)
-            return
-        print(self.value)
-        self.next.show()
-    
-    def delete(self, value, prev = None):
-        if self.value == value:
-            if prev == None:
-                self = None
-            else:
-                prev.next = self.next
-        elif self.next == None:
-            return
-        else:
-            self.next.delete(value, self)
-
-
-x = linked_list(1)
-x.insert(2)
-x.insert(3)
-x.insert(4)
-
-
-
-# Attempt 2
-
+# Node class
 class Node():
     def __init__(self, value):
         self.value = value
         self.next = None
-
+        
+# Linked list class
 class LinkedList():
     def __init__(self):
         self.head = None
-
-    def insert(self, value):
-        temp = self.head
-        self.head = Node(value)
-        self.head.next = temp
     
     def show(self):
         c = self.head
         while c:
             print(c.value)
             c = c.next
-    
+        
+    def array(self):
+        arr = []
+        c = self.head
+        while c:
+            arr.append(c.value)
+            c = c.next
+        print(arr)
+            
     def search(self, value):
         c = self.head
         while c:
             if c.value == value:
                 return True
             c = c.next
-        return False
+        return False            
+
+    def insert(self, value):
+        temp = self.head
+        self.head = Node(value)
+        self.head.next = temp
     
     def delete(self, value):
         prev = None
         c = self.head
         while c:
             if c.value == value:
-                prev.next = c.next
+                if prev:
+                    prev.next = c.next
+                else:
+                    self.head = c.next
                 return
+            prev = c
+            c = c.next
             
-        
-
-x = LinkedList()
-for i in range(0,10):
-    x.insert(i)
+            
+# Main function (first version, using functional programming)
+# def words(file):
+#     with open(file) as file_object:
+#         contents = file_object.read().strip()
+#         d = LinkedList()
+#         word = ''
+#         for c in contents:
+#             if c in skip:
+#                 if word and not d.search(word):
+#                     d.insert(word)
+#                 word = ''
+#                 continue
+#             else:
+#                 word += c
+#     d.array()
