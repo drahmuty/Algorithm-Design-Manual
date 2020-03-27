b238d035ef028dc9a081d37a7b0a150b3b7921cb
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-03-26 22:20:48 -0500

    Update 03-17.py

--- 03-17.py
+++ 03-17.py
@@ -1,17 +1,17 @@
-# Main program
-def main(x):
+# Encrypt file
+def encrypt(x):
     with open('file.txt') as file_obj:
         contents = file_obj.read().strip()
     encrypted_contents = cshift(contents, x)
     with open('encrypted_file.txt', 'w') as file_w:
         file_w.write(encrypted_contents)
-    doc = Doc('encrypted_file.txt')
-    t = HashTable()
-    w = doc.next_word()
-    while w:
-        t.insert(w)
-        w = doc.next_word()
-    print(t.print_items())
+    # doc = Doc('encrypted_file.txt')
+    # t = HashTable()
+    # w = doc.next_word()
+    # while w:
+    #     t.insert(w)
+    #     w = doc.next_word()
+    # print(t.print_items())
 
 # Decryption program
 def decrypt():
@@ -21,13 +21,12 @@ def decrypt():
         contents = file_obj.read().strip()
     
     # Count each letter and store in dictionary
-    letter_dict = Dict()
+    letter_table = HashTable()
     for c in contents:
-        if ord(c) > 64 and ord(c) < 91:
-            letter_dict.insert(c)
+        letter_table.insert(c)
     
     # Print dictionary
-    print(letter_dict.dict)
+    print(letter_table.dict)
 
     # Insert dictionary items into binary tree
 
@@ -38,24 +37,7 @@ def decrypt():
 
     # Print contents of file
     
-
-# Dictionary data structure
-class Dict:
-    def __init__(self):
-        self.dict = {}
-        self.keys = self.dict.keys()
     
-    def search(self, x):
-        if x in self.keys:
-            return True
-        else:
-            return False
-    
-    def insert(self, x):
-        if self.search(x):
-            self.dict[x] += 1
-        else:
-            self.dict[x] = 1
 
 # Caesar shift (text = string; x = integer shift value)
 def cshift(text, x):
@@ -68,43 +50,35 @@ def cshift(text, x):
         text_shifted += t
     return text_shifted
 
-# Hash table class
+# Hash table class for storing letter count
 class HashTable:
     def __init__(self):
-        self.size = 1000
-        table = [[] for a in range(self.size)]
+        self.size = 26
+        table = [0 for a in range(self.size)]
         self.dict = table
-        self.item_count = 0
 
     def hash(self, x):
-        return hash(x) % self.size
+        return (ord(x) - 65) % self.size
 
     def insert(self, x):
-        if self.search(x):
+        if ord(x) < 65 or ord(x) > 90:
             return
         k = self.hash(x)
-        self.dict[k].append(x)
-        self.item_count += 1
+        self.dict[k] += 1
 
     def search(self, x):
         k = self.hash(x)
-        for v in self.dict[k]:
-            if v == x:
-                return True
-        return False
+        if self.dict[k] > 0:
+            return True
+        else:
+            return False
     
     def delete(self, x):
         if not self.search(x):
             print(str(x) + ' not found')
-            return
-        k = self.hash(x)
-        self.dict[k].remove(x)
-        self.item_count -= 1
-
-    def print_items(self):
-        for x in self.dict:
-            for y in x:
-                print(y)
+        else:
+            k = self.hash(x)
+            self.dict[k] = 0
 
 # Document class
 class Doc():
