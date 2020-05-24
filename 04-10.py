4a54931a31da356f112c07420d5c42c44751d2e7
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-05-24 10:49:53 -0500

    Create 04-10.py

---
+++ 04-10.py
@@ -0,0 +1,21 @@
+"""
+4-10. Given a set S of n integers and an integer T, 
+give an O(nk−1logn) algorithm to test whether k of 
+the integers in S add up to T.
+"""
+
+"""
+inputs:
+s (list of integers)
+t (target sum)
+k (number of addends)
+
+output:
+true or false
+or return addends?
+
+program steps:
+sort s
+
+
+"""
