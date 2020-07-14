a1a5f05cc6ebd871179625811c1be62702b99903
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-07-13 22:13:59 -0500

    Update 4-37.py

--- 4-37.py
+++ 4-37.py
@@ -1,4 +1,4 @@
-# Selection sort
+# Selection sort (first attempt)
 def selection_sort(A):
     sorted = []
     while A:
@@ -9,3 +9,15 @@ def selection_sort(A):
                 min_index = i
         sorted.append(A.pop(min_index))
     return sorted
+
+# Selection sort (correct solution)
+def selection_sort(A):
+
+    for i in range(len(A)):
+        min_index = i
+        
+        for j in range(i+1, len(A)):
+            if A[j] < A[min_index]:
+                min_index = j
+        
+        A[i], A[min_index] = A[min_index], A[i]
