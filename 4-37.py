26727648941d21a29f0480e38dcb641065a82bfb
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-07-13 22:14:16 -0500

    Update 4-37.py

--- 4-37.py
+++ 4-37.py
@@ -12,12 +12,9 @@ def selection_sort(A):
 
 # Selection sort (correct solution)
 def selection_sort(A):
-
     for i in range(len(A)):
         min_index = i
-        
         for j in range(i+1, len(A)):
             if A[j] < A[min_index]:
                 min_index = j
-        
         A[i], A[min_index] = A[min_index], A[i]
