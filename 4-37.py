786b142acefe5b2ee196b87aa580381d9fb0ba89
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-07-14 21:37:22 -0500

    Update 4-37.py

--- 4-37.py
+++ 4-37.py
@@ -10,7 +10,7 @@ def selection_sort(A):
         sorted.append(A.pop(min_index))
     return sorted
 
-# Selection sort (correct solution)
+# Selection sort (standard solution)
 def selection_sort(A):
     for i in range(len(A)):
         min_index = i
@@ -28,3 +28,11 @@ def insertion_sort(A):
                 i -= 1
             else:
                 break
+
+# Insertion sort (standard solution)
+def insertion_sort(A):
+    for i in range(len(A)):
+        j = i
+        while j > 0 and A[j] < A[j-1]:
+            A[j], A[j-1] = A[j-1], A[j]
+            j -= 1
