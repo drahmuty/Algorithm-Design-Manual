dca8da473e06ef1cdf62b1192a8603868400d93f
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-07-27 20:37:49 -0500

    Update 4-37.py

--- 4-37.py
+++ 4-37.py
@@ -124,3 +124,23 @@ def merge(s, low, mid, high):
     while buffer2:
         s[i] = buffer2.pop(0)
         i += 1
+
+# Quicksort
+def quicksort(s):
+    quicksort_recur(s, 0, len(s)-1)
+
+def quicksort_recur(s, l, h):
+    if l < h:
+        p = partition(s, l, h)
+        quicksort_recur(s, l, p-1)
+        quicksort_recur(s, p+1, h)
+
+def partition(s, l, h):
+    p = h
+    firsthigh = l
+    for i in range(l, h):
+        if s[i] < s[p]:
+            s[i], s[firsthigh] = s[firsthigh], s[i]
+            firsthigh += 1
+    s[p], s[firsthigh] = s[firsthigh], s[p]
+    return firsthigh
