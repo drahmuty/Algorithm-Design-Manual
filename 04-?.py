5e903380da7436f0c8bc3062dfe89bf5d49ddd68
drahmuty <35937398+drahmuty@users.noreply.github.com>
2020-08-24 21:14:41 -0500

    Update 04-?.py

--- 04-?.py
+++ 04-?.py
@@ -2,12 +2,10 @@
 Flapjack challenge.
 http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.111.2131&rep=rep1&type=pdf
 """
-
 def flapjack(stack):
     
     # Return list if sorted
     if(all(stack[i] <= stack[i + 1] for i in range(len(stack)-1))):
-        print('sorted')
         return stack
 
     # Find max index
@@ -27,9 +25,18 @@ def flapjack(stack):
     bottom = stack[max_index+1:]
     top.reverse()
     stack = top + bottom
+    print('Flip at ' + str(max_index))
     
     # Flip to bottom
     stack.reverse()
+    print('Flip at ' + str(len(stack)-1))
 
-    # Sort remaining stack and cobine with sorted bottom
+    # Combine result into final list
     return flapjack(stack[:len(stack)-1]) + stack[len(stack)-1:]
+
+
+# Test cases
+a = [1,2,3,4,5]
+b = [5,4,3,2,1]
+c = [5,1,2,3,4]
+d = [5,2,1,4,3,9,6,8,7]
