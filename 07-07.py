7b81a9d05ddbdca69beae0c39a9aac5d8c1169c5
drahmuty <35937398+drahmuty@users.noreply.github.com>
2021-01-24 12:12:52 -0600

    Update 07-07.py

--- 07-07.py
+++ 07-07.py
@@ -1,10 +1,16 @@
 """Bandwidth minimization."""
 
 
-
+import math
 from collections import defaultdict
 
-
+# Solution class.
+class Solution:
+    def __init__(self, n):
+        self.solution = [None for i in range(n+1)]
+        self.bandwidth = n-1
+        self.solved = False
+        self.final_solution = None
 
 # Matrix representation of a graph.
 class Graph:
@@ -13,6 +19,8 @@ class Graph:
         self.n_vertices = n
         self.n_edges = 0
         self.graph = [[0 for i in range(n)] for i in range(n)]
+        self.min_band = None
+        self.max_band = n - 1
 
     def print(self):
         for i in self.graph:
@@ -28,82 +36,135 @@ class Graph:
     def cost(self, x, y):
         pass
 
+    # Get largest min bandwidth for graph vertices.
+    # This value is the lower bound for the bandwidth solution.
+    def get_min_band(self):
+        min = None
+        for v in self.graph:
+            degree = 0
+            for y in v:
+                degree += y
+            bandwidth = math.ceil(degree / 2)
+            if not min or bandwidth > min:
+                min = bandwidth
+            if min == 1:
+                break
+        self.min_band = min
+        return min
 
-
-# Find the max edge length in a linear ordering of vertices.
-def max_distance(line, graph):
+# Find the max bandwidth in a linear ordering of vertices.
+def max_band(sol, graph):
     max = 0
-    k = len(line)
-    for i in range(k-1):
-        x = line[i]
-        for j in range(i+1, k):
-            y = line[j]
+    k = len(sol)
+    for i in range(k-1, -1, -1):
+        x = sol[i]
+        for j in range(i-1, -1, -1):
+            y = sol[j]
             if graph.graph[x-1][y-1]:
-                d = j - i
-                if d > max:
-                    max = d
+                b = i - j
+                if b > max:
+                    max = b
     return max
 
-
-
-# Backtracking algorithm and helper functions.
 def backtrack(a, k, data):
+    if a.solved:
+        return
     if is_a_solution(a, k, data):
         process_solution(a, k, data)
     else:
         k += 1
         candidates = construct_candidates(a, k, data)
         for c in candidates:
-            a[k] = c
+            a.solution[k] = c
             backtrack(a, k, data)
-            a[k] = None
+            a.solution[k] = None
 
 def is_a_solution(a, k, data):
-    return k == len(a)-1
+    return k == len(a.solution)-1
 
 def process_solution(a, k, data):
-    solution = a[1:]
-    print(solution)
-    print(max_distance(solution, data))
+    solution = a.solution[1:]
+    max = None
+    k = len(solution)
+    for i in range(k-1):
+        x = solution[i]
+        for j in range(i+1, k):
+            y = solution[j]
+            if data.graph[x-1][y-1]:
+                d = j - i
+                if not max or d > max:
+                    max = d
+                if max == MAX:
+                    break
+    if max == MIN or MIN == MAX:
+        a.final_solution = solution
+        a.bandwidth = max
+        a.solved = True
+    elif max < a.bandwidth:
+        a.final_solution = solution
+        a.bandwidth = max
 
 def construct_candidates(a, k, data):
+    partial_sol = a.solution[1:k]
     candidates = []
     in_sol = defaultdict(bool)
     for i in range(1, k):
-        in_sol[a[i]] = True
+        in_sol[a.solution[i]] = True
     for i in range(len(data.graph)):
-        if not in_sol[i+1]:
-            candidates.append(i+1)
+        c = i+1
+        if k == 1:
+            candidates.append(c)
+        elif not in_sol[c]:
+            if partial_sol:
+                p = partial_sol + [c]
+                b = max_band(p, data)
+                if b > a.bandwidth:
+                    # print('skipping', c, '('+str(b)+' > '+str(a.bandwidth)+')')
+                    continue
+                candidates.append(c)
     return candidates
 
+# MAIN PROGRAM - Find the minimum bandwidth of a graph.
+def min_bandwidth(graph):
+
+    print('GRAPH:')
+    graph.print()
+
+    global MIN, MAX
+    MIN = graph.get_min_band()
+    MAX = graph.max_band
+
+    solution = Solution(graph.n_vertices)
+
+    backtrack(solution, 0, graph)
+
+    print('SOLUTION:', solution.final_solution)
+    print('MAX BANDWIDTH:', solution.bandwidth)
+    print()
+    return solution.final_solution, solution.bandwidth
 
 
 
+# Test cases.
+a = Graph(4)
+a.add_edge(1,2)
+a.add_edge(1,3)
+a.add_edge(1,4)
+min_bandwidth(a)
 
+b = Graph(5)
+b.add_edge(1,2)
+b.add_edge(1,3)
+b.add_edge(1,4)
+b.add_edge(1,5)
+min_bandwidth(b)
 
-
-# a = Graph(8)
-# a.add_edge(1,8)
-# a.add_edge(8,2)
-# a.add_edge(2,7)
-# a.add_edge(7,3)
-# a.add_edge(3,6)
-# a.add_edge(6,4)
-# a.add_edge(4,5)
-# a.print()
-# backtrack([None for i in range(9)], 0, a)
-
-
-# b = Graph(5)
-# b.add_edge(1,2)
-# b.add_edge(1,3)
-# b.add_edge(1,4)
-# b.add_edge(1,5)
-# b.print()
-
-c = Graph(4)
-c.add_edge(1,2)
-c.add_edge(1,3)
-c.add_edge(1,4)
-c.print()
-backtrack([None for i in range(5)], 0, c)
+c = Graph(8)
+c.add_edge(1,8)
+c.add_edge(8,2)
+c.add_edge(2,7)
+c.add_edge(7,3)
+c.add_edge(3,6)
+c.add_edge(6,4)
+c.add_edge(4,5)
+min_bandwidth(c)
