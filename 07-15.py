f812fcebf6d5e4c799b8de5476fb85b15170eb20
drahmuty <35937398+drahmuty@users.noreply.github.com>
2021-02-11 21:09:46 -0600

    Update 07-15.py

--- 07-15.py
+++ 07-15.py
@@ -1,54 +1,35 @@
-"""List all k element subsets of a set of n items."""
+"""List all k-element subsets of a set of n items."""
 
-from collections import defaultdict
 
-def k_subsets1(set_items, k):
-    backtrack(defaultdict(bool), 0, set_items, k)
-def backtrack1(solution, counter, set_items, k):
-    # print(counter, solution)
-    if counter == k:
-        print('SOLUTION', solution)
+
+def k_subsets(n, k):
+    a = [0 for i in range(n)]
+    backtrack(a, -1, n, k, 0)
+
+def backtrack(solution, counter, n, k, sum):
+    if sum == k:
+        subset = []
+        for s in range(n):
+            if solution[s] == 1:
+                subset.append(s + 1)
+        print(subset)
+    elif counter == n - 1:
+        return
+    elif n - (counter + 1) < k - sum:
+        return
     else:
         counter += 1
-        candidates = set()
-        for item in set_items:
-            if not item in solution:
-                candidates.add(item)
-        # print('CANDIDATES', candidates)
+        candidates = [1, 0]
         for c in candidates:
-            solution.add(c)
-            backtrack(solution, counter, set_items, k)
-            solution.remove(c)
-
-
-
-def k_subsets(k, items):
-    print('Subsets of ' + str(items) + ' (k = ' + str(k) + ')')
-    backtrack([None for i in range(k)], -1, k, sorted(items))
-    print()
-
-def backtrack(solution, j, k, items):
-    if j + 1 == k:
-        print(solution)
-    else:
-        j += 1
-        in_solution = defaultdict(bool)
-        for s in solution:
-            in_solution[s] = True
-        candidates = []
-        for item in items:
-            if j == 0:
-                candidates.append(item)
-            elif not in_solution[item] and item > solution[j - 1]:
-                candidates.append(item)
-        for c in candidates:
-            solution[j] = c
-            backtrack(solution, j, k, items)
-            solution[j] = None
+            solution[counter] = c
+            sum += c
+            backtrack(solution, counter, n, k, sum)
+            solution[counter] = 0
+            sum -= c
 
 
 
 # Test cases.
-k_subsets(2, [1, 2, 3])
-k_subsets(2, [1, 2, 3, 4])
-k_subsets(3, [1, 2, 3, 4, 5])
+k_subsets(3, 2)
+k_subsets(5, 3)
+k_subsets(10, 5)
