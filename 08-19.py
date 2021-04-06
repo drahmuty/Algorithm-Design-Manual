aa902b7ffc7eaf9021e0b6e726e3176e77979b5f
drahmuty <35937398+drahmuty@users.noreply.github.com>
2021-04-05 20:23:30 -0500

    Create 08-19.py

---
+++ 08-19.py
@@ -0,0 +1,39 @@
+"""Minimize bookcase height with n books of variable height and width and max shelf width."""
+
+def bookshelves(books, width):
+    
+    B = [None] + books
+    n = len(B)
+    C = [float('inf')] * n
+    C[0] = 0
+    
+    # Evaluate all books.
+    for i in range(1, n):
+        j = i
+        h = 0
+        w = 0
+        
+        # Find optimal solution for current and preceding books.
+        while j > 0:
+            
+            # Make sure current book fits within shelf width.
+            w += B[j][0]
+            if w > width:
+                break
+            
+            # Update shelf height if current book is bigger.
+            h = max(h, B[j][1])
+            
+            # Choose arrangement that minimized overall bookcase height.
+            C[i] = min(C[i], C[j-1] + h)
+            
+            # Move on to next book.
+            j -= 1
+            
+    print(C)
+
+    
+# Test case.
+books = [[1,2], [1,1], [1,1], [1,2], [1,2]]
+width = 2
+bookshelves(books, width)
