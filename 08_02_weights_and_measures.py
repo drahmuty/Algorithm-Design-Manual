70bf6536e9e0644d47f0067cb4075c1fb227e9a6
drahmuty <35937398+drahmuty@users.noreply.github.com>
2021-04-20 21:00:16 -0500

    Update 08_02_weights_and_measures.py

--- 08_02_weights_and_measures.py
+++ 08_02_weights_and_measures.py
@@ -4,6 +4,64 @@ Full problem description: https://onlinejudge.org/external/101/10154.pdf
 """
 
 
+
+# ATTEMPT 2
+
+def turtles(T):
+
+    # Sort list by decreasing weight and strength.
+    T.sort(reverse=True, key=lambda x: (x[0], x[1]))
+
+    # Add empty item to T for easier indexing.
+    T = [None] + T
+    n = len(T)
+
+    # Dynamic solution list.
+    C = [0] * n    # capacity
+    D = [0] * n    # distance
+
+    # Main loop.
+    for i in range(1, n):
+        w1 = T[i][0]
+        c1 = T[i][1] - w1
+        if c1 < 0:
+            continue
+        C[i] = c1
+        D[i] = 1
+        for j in range(1, i):
+            w2 = T[j][0]
+            c2 = T[j][1] - w2
+            add_cap = min(c1, C[j] - w1)
+            new_cap = min(c1, c2 - w1)
+            if add_cap < 0 and new_cap < 0:
+                continue
+            if add_cap >= new_cap and D[j] >= D[i]:
+                C[i] = add_cap
+                D[i] = D[j] + 1
+            if new_cap > C[i] and D[i] <= 2:
+                C[i] = new_cap
+                D[i] = 2
+
+    return max(D)
+
+
+
+
+T = [
+    (300, 1000),
+    (1000, 1200),
+    (200, 600),
+    (100, 101),
+    (2000, 2300)
+]
+print(turtles(T))
+
+
+
+
+
+# ATTEMPT 1
+
 def turtles(T):
 
     # Sort T by decreasing weight and strength.
